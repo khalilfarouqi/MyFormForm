8432bf4a8137173ea30f43f00cf51c2be868091f
khalilfarouqi <bio.khalil.f@gmail.com>
2024-02-09 16:12:09 +0100

    récupérer les données envoyées par le formulaire

--- myform_form/myform_form/views.py
+++ myform_form/myform_form/views.py
@@ -3,10 +3,12 @@ from .forms import SignupForm, SignupFormWidget, SignupFormData
 # Create your views here.
 def signup10_affichage(request) :
     form = SignupForm()
-    return render(request, "myform_form/signup10_affichage.html", context={"form" : form})
+    return render(request, "myform_form/signup10_affichage.html", context={"form": form})
 def signup20_widget(request) :
     form = SignupFormWidget()
-    return render(request, "classroom/signup20_widget.html", context={"form" : form})
-def signup30_data(request) :
+    return render(request, "classroom/signup20_widget.html", context={"form": form})
+def signup30_data(request):
     form = SignupFormData()
-    return render(request, "myform_form/signup30_data.html", context={"form" : form})
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, "myform_form/signup30_data.html", context={"form": form})
