8bcebe5762f88ce14d8370a0fbca798d55ec7aaa
khalilfarouqi <bio.khalil.f@gmail.com>
2024-02-09 16:13:05 +0100

    clean code

--- myform_form/myform_form/views.py
+++ myform_form/myform_form/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .forms import SignupForm, SignupFormWidget, SignupFormData
+
+
 # Create your views here.
-def signup10_affichage(request) :
+def signup10_affichage(request):
     form = SignupForm()
     return render(request, "myform_form/signup10_affichage.html", context={"form": form})
-def signup20_widget(request) :
+
+
+def signup20_widget(request):
     form = SignupFormWidget()
     return render(request, "classroom/signup20_widget.html", context={"form": form})
+
+
 def signup30_data(request):
     form = SignupFormData()
     if request.method == "POST":
